4a3a38a84294c426a3828eb813777b4c0722b565
Enzo <enzonbarata@outlook.com>
2026-04-04 23:01:49 -0300

    docs(svelte): add dark mode support

--- packages/svelte/.storybook/preview.ts
+++ packages/svelte/.storybook/preview.ts
@@ -1,18 +1,27 @@
+import { themes } from 'storybook/theming';
+import '../src/routes/layout.css'; // ajuste o caminho para o seu CSS
+
 import type { Preview } from '@storybook/sveltekit';
 
 const preview: Preview = {
+	decorators: [
+		(Story, context) => {
+			const isDark = context.globals.backgrounds?.value === 'dark';
+			document.body.classList.toggle('dark', isDark);
+			return Story();
+		}
+	],
 	parameters: {
+		docs: {
+			theme: themes.dark
+		},
 		controls: {
 			matchers: {
 				color: /(background|color)$/i,
 				date: /Date$/i
 			}
 		},
-
 		a11y: {
-			// 'todo' - show a11y violations in the test UI only
-			// 'error' - fail CI on a11y violations
-			// 'off' - skip a11y checks entirely
 			test: 'todo'
 		}
 	}
