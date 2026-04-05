be850c388aa302e1ffd32d1d5311fa4f19b0040f
Enzo <enzonbarata@outlook.com>
2026-04-04 23:02:35 -0300

    docs(svelte): storybook

---
+++ packages/svelte/src/lib/components/button/Button.stories.svelte
@@ -0,0 +1,23 @@
+<script module>
+	import { defineMeta } from '@storybook/addon-svelte-csf';
+	import Button from './Button.svelte';
+	import { fn } from 'storybook/test';
+
+	const { Story } = defineMeta({
+		title: 'Buttons/Button',
+		component: Button,
+		tags: ['autodocs'],
+		args: {
+			onclick: fn()
+		},
+		parameters: {
+			docs: {
+				controls: {
+					exclude: ['onclick']
+				}
+			}
+		}
+	});
+</script>
+
+<Story name="Default">Button</Story>
